1825351e37c3830b7704e19abb156ba22a64d1bf
Constantine Anikin <anikinator@gmail.com>
2018-03-29 15:07:15 +0300

    ntp_install.sh, ntp_verify - betta. Whitespace at paths need to be checked.

--- ntp_deploy.sh
+++ ntp_deploy.sh
@@ -17,20 +17,18 @@ fi
 #sed -i.backup '/pool/d' /etc/ntp.conf
 if [ -f /etc/ntp.conf ]
 	then
-		sed -i.backup '/pool /d; /more information/a pool ua.pool.ntp.org iburst' /etc/ntp.conf
-
+		sed -i.original '/pool /d; /more information/a pool ua.pool.ntp.org iburst' /etc/ntp.conf
+		cp /etc/ntp.conf /etc/ntp.conf.backup
 		systemctl restart ntp
 fi
 
 # Check and install ntp_verify.sh at crontab ///////////////////////////////////
 WD=`pwd`
+chmod +x "$WD/ntp_verify.sh"
 CTP='/var/spool/cron/crontabs/root'
 PATH="PATH=$WD:$PATH"
-CRONJOB="* * * * * /bin/bash $WD/ntp_verify.sh"
+CRONJOB="* * * * * $WD/ntp_verify.sh"
 #echo "${PATH}";"${WD}/ntp_verify.sh" | crontab -
-#(echo -e "${PATH}${CRONJOB}") | crontab -
-#echo "$CRONJOB" | crontab -
-#$CRONS=`crontab -l`
 echo "$PATH" > "$CTP"
 #echo $CRONS >> "$CTP" 
 echo "$CRONJOB" >> "$CTP"
--- ntp_verify.sh
+++ ntp_verify.sh
@@ -10,3 +10,14 @@ if [ -n "$NTPState" -a "$NTPState" != "active" ]
 	then
 		systemctl start ntp
 fi
+
+# Get changes and restore ntp.conf
+if [ -f /etc/ntp.conf.backup ]
+ then
+	GETDIFF=`diff /etc/ntp.conf /etc/ntp.conf.backup`
+	if [ -n "$GETDIFF" ]
+	 then
+		echo $GETDIFF
+		cp /etc/ntp.conf.backup /etc/ntp.conf
+	fi
+fi
